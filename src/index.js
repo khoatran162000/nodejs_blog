c42cdc86619b254f1610dee89e8f3bf9685b5e08
khoatran162000 <khoatranchemistry@gmail.com>
2021-07-29 12:31:23 +0700

    read from db

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
-const app = express();
-const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,12 +31,12 @@ app.engine(
         extname: '.hbs',
     }),
 );
-			app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+	app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
